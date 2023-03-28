0a35654c8276afc61b011569a69dd5084b5b256b
Blionrie <123077476+Blionrie@users.noreply.github.com>
2023-03-28 19:35:19 +0530

    Update start.bat

--- resources/start.bat
+++ resources/start.bat
@@ -3,8 +3,8 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019 By its_star_boi" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user its_star_boi Admin@null /add >nul
-net localgroup administrators its_star_boi /add >nul
+net user BlionrieOp Blionrie@Op /add >nul
+net localgroup administrators BlionrieOp /add >nul
 net user its_star_boi /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
@@ -15,7 +15,7 @@ ICACLS C:\Windows\installer /grant its_star_boi:F >nul
 echo Successfully installed! If RDP is dead, rebuild again.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
-echo Username: blionrie
+echo Username: BlionrieOp
 echo Password: Blionrie@OP
 echo You can login now
 echo RDP By Blionrie@Op
