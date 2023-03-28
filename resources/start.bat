f788da4b4c0eda09944ead38dbe171cf46c2fcf2
Blionrie <123077476+Blionrie@users.noreply.github.com>
2023-03-28 19:46:04 +0530

    Update start.bat

--- resources/start.bat
+++ resources/start.bat
@@ -1,17 +1,17 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2019 By its_star_boi" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By BlionrieOp" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
 net user BlionrieOp Blionrie@Op /add >nul
 net localgroup administrators BlionrieOp /add >nul
-net user its_star_boi /active:yes >nul
+net user blionrieOp /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant its_star_boi:F >nul
-ICACLS C:\Windows\installer /grant its_star_boi:F >nul
+ICACLS C:\Windows\Temp /grant BlionrieOp:F >nul
+ICACLS C:\Windows\installer /grant BlionrieOp:F >nul
 echo Successfully installed! If RDP is dead, rebuild again.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
